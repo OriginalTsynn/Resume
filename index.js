28d7b1e3702a89c78ade4109deda209beb4bf6c6
Nathan Rotach <Nathan.Rotach@gmail.com>
2019-04-22 12:26:01 -0400

    rainbows

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 #! /usr/bin/env node
+var colors = require('colors')
+
 const fs = require('fs')
 
 fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
   if (err) {
     console.log(err)
   } else {
-    console.log(data)
+    console.log(data.rainbows)
     return data
   }
 })
