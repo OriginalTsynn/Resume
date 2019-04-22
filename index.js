f593d61c09e2bc3968e217a6bdf34fe40550c484
Nathan Rotach <Nathan.Rotach@gmail.com>
2019-04-22 12:30:23 -0400

    removal of attempted colorization due to failures

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
   if (err) {
     console.log(err)
   } else {
-    console.log(data.rainbows)
-    return data.rainbows
+    console.log(data)
+    return data
   }
 })
